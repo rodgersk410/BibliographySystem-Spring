321d1abe276b827a4eeb87903e5c5d5a370185db
rodgersk410 <rodgersk410@gmail.com>
2017-04-02 18:58:27 -0400

    added file upload request (which doesn't do anything yet)

--- src/main/java/webProject/HomeController.java
+++ src/main/java/webProject/HomeController.java
@@ -8,9 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+
 
 @Controller
 public class HomeController {
@@ -85,6 +88,17 @@ public class HomeController {
             Model model) {
         jdbcTemplate.update("update entries set author=?, title=?, year=?, journal=? "
         		+ "where id = ?", a, t, y, j, id);
+        return "redirect:/biblio"; // back to the biblio view
+    }
+    
+    //import bibtex files
+    @RequestMapping(value = "/uploadFile")
+    public String uploadFile(@RequestParam("file") MultipartFile file, Model model, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            System.out.println("file empty");
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+        }
+        
         return "redirect:/biblio"; // back to the biblio view
     }
     
