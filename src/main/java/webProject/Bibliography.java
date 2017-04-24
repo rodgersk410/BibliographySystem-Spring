0b76f2a4cc959d57a567057ea49065223e7c4199
rodgersk410 <rodgersk410@gmail.com>
2017-04-24 01:45:15 -0400

    part of last commit

--- src/main/java/webProject/Bibliography.java
+++ src/main/java/webProject/Bibliography.java
@@ -1,12 +1,53 @@
 package webProject;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
+
+@XmlRootElement(name="root")
+@XmlAccessorType(XmlAccessType.FIELD)
+class Root {
+	
+	@XmlElement(name = "document")
+	List<Bibliography> bibliographies;
+	
+	@XmlElement(name = "totalfound")
+	TotalFound totalFound;
+	
+	@XmlElement(name = "totalsearched")
+	TotalSearched totalSearched;
+	
+	List<Bibliography> getBibliographies() {
+		return this.bibliographies;
+	}
+	
+}
+
+@XmlRootElement
+class TotalFound {}
+
+@XmlRootElement
+class TotalSearched {}
+
+@XmlRootElement(name="document")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Bibliography {
 
+	@XmlElement(name = "authors")
     private String author;
-    private String title;
-    private int year;
-    private String journal;
-    private final int id;
+	
+	@XmlElement(name = "title")
+	private String title;
+	@XmlElement(name = "py")
+	private int year;
+	@XmlElement(name = "publisher")
+	private String journal;
+	
+	private int id;
+	private int searchId;
     
     public Bibliography(int id, String author, String title, int year, String journal) {
     	this.author = author;
@@ -16,8 +57,14 @@ public class Bibliography {
     	this.id = id;
     }
     
+    public Bibliography(){}
+    
     public int getId() {
         return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getAuthor() {
@@ -50,6 +97,14 @@ public class Bibliography {
 
     public void setJournal(String journal) {
         this.journal = journal;
+    }
+    
+    public int getSearchId() {
+        return searchId;
+    }
+    
+    public void setSearchId(int searchId) {
+    	this.searchId = searchId;
     }
 
 }
